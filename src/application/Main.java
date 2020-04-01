5da329c80015be19fd20b5a779fc91eca4543d6b
LUNKUS <sigitas@lunkus.com>
2020-04-01 08:47:12 +0300

    set up menus

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,13 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    // Importing menuBar, menu, openItem
     MenuBar menu;
     Menu fiileMenu;
+    MenuItem openItem;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,11 +25,23 @@ public class Main extends Application {
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 400, 400);
 
+        // Instializing menuBar, fileMenu, openItem
         menu = new MenuBar();
         fiileMenu = new Menu("file");
+        openItem = new MenuItem("open");
 
+        // Add openItem to the fileMenu
+        fiileMenu.getItems().add(openItem);
+
+        // Add fileMenu to the menu
         menu.getMenus().add(fiileMenu);
 
+        // Action perform when clicked on openItem
+        openItem.setOnAction((e) -> {
+            System.out.println("clicked open");
+        });
+
+        // Add menuBar to the root
         root.setTop(menu);
 
         primaryStage.setScene(scene);
